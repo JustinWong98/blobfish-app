5efec0e5a38737adf58d08f7837a8638420a1ec0
JustinWong98 <justinwong8991@gmail.com>
2021-11-21 12:05:08 +0800

    added simple 3d bg, used gltf model for blobfish

---
+++ src/components/Avatars/Light.jsx
@@ -0,0 +1,14 @@
+export const Light = ({ brightness, color }) => {
+  return (
+    <rectAreaLight
+      width={3}
+      height={3}
+      color={color}
+      intensity={brightness}
+      position={[-2, 0, 5]}
+      lookAt={[0, 0, 0]}
+      penumbra={1}
+      castShadow
+    />
+  );
+}
